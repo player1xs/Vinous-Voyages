0b2527a5313f659c0eaf1093b3f5a9b8ba5e2dd3
jamesbraid <james_braid@hotmail.co.uk>
2023-12-06 15:33:48 +0000

    React router set up in main.jsx with paths for components. Files created for components in components folder.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/wineryIndex',
+        element: <WineryIndex />
+      },      {
+        path: '/wineryIndex/:wineryId',
+        element: <WinerySingle />
+      },      {
+        path: '/register',
+        element: <Register />
+      },      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
