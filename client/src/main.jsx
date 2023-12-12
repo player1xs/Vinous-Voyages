97f20523494b2893c3a8912a44b8ee7765c88f55
Nasiim Nuur <nasiimmo.691@gmail.com>
2023-12-12 18:07:36 +0000

    nav-bar bootstrap

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/main.scss'
 
 // Page components
