0b2eab73125b63d826ff2aa0f07cd0974cca68e5
Nasiim Nuur <nasiimmo.691@gmail.com>
2023-12-12 12:57:07 +0000

    css for nav

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,4 @@
+
 export default function Footer(){
   return (
     <footer className='text-center mb-2 mt-4'>
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,12 +4,24 @@ import { Link, useActionData, useNavigate } from 'react-router-dom'
 import Model from 'react-bootstrap/Modal'
 import Modal from 'react-bootstrap/Modal'
 
-// import { FaCircleUser } from 'react-icons/fa6'
+
 import Button from 'react-bootstrap/Button'
 import userIcon from '../images/image.png'
 import { Form } from 'react-router-dom'
+
+
 export default function Nav() {
 
+
+  const toggleModal = () => {
+    // Toggle the state to show/hide the register modal
+    setModalShow((prevShow) => !prevShow)
+  }
+
+  const toggleNav = () => {
+    // Toggle the state to show/hide the navigation menu
+    setShow((prevShow) => !prevShow)
+  }
 
   const res = useActionData()
   console.log(res)
@@ -25,79 +37,74 @@ export default function Nav() {
   const [show, setShow] = useState(false)
   const [modalShow, setModalShow] = useState(false)
   const [loginModalShow, setLoginModalShow] = useState(false)
+
   return (
     <>
       <header>
-        <h1><Link to='/'>Vinous Voyages</Link></h1>
-
-        <button className='nav-toggle' onClick={() => setShow(true)}>
+        <button className='nav-toggle' onClick={toggleNav}>
           <span></span>
           <span></span>
           <span></span>
         </button>
-        <Button className="me-4 mb-4" onClick={() => setModalShow(true)}>
-          <Link to='/'><img className='logo' src={userIcon} height='50' alt='user icon' /></Link>
+        <h1><Link to='/'>Vinous Voyages</Link></h1>
+        <Button className="icon" onClick={toggleModal}>
+          <img className='logo' src={userIcon} height='50' alt='user icon' />
         </Button>
       </header>
       <Model show={show} fullscreen={true} onHide={() => setShow(false)}>
         <Model.Header closeButton>
           <nav onClick={() => setShow(false)}>
-            <Link to='/'>Home</Link>&nbsp;
-            <Link to='/wineryIndex'>Winery Index</Link>&nbsp;
-            <Link to='/createWinery'>Create Winery</Link>&nbsp;
+              <Link to='/'>Home</Link>&nbsp;
+              <Link to='/wineryIndex'>Winery Index</Link>&nbsp;
+              <Link to='/createWinery'>Create Winery</Link>&nbsp;
             {/* <Link to='/login'>Log-in</Link>&nbsp;
             <Link to='/register'>Register</Link>&nbsp; */}
           </nav>
         </Model.Header>
       </Model>
 
-
-      <Modal show={modalShow} halfscreen={true} onHide={() => setModalShow(false)}>
+      <Modal show={modalShow} halfscreen={true} onHide={() => setModalShow(false)} className='centered-modal'>
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">Sign up</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form className='form' method='POST'>
-            <input type='text' name='username' placeholder='Username' />
-            <input type='email' name='email' placeholder='Email' />
-            <input type='password' name='password' placeholder='Password' />
-            <input type='password' name='passwordConfirmation' placeholder='confirm password' />
-            <button className='btn btn-grey' type='submit'>register</button>
+          <Form className='create' method='POST'>
+            <h1 className='text-center bold display-3 mb-4'>sign-up</h1>
+            <input type='text' name='username' placeholder='Username...' />
+            <input type='email' name='email' placeholder='Email...' />
+            <input type='password' name='password' placeholder='Password...' />
+            <input type='password' name='passwordConfirmation' placeholder='confirm password...' />
+            <button className='register' type='submit'>register</button>
             {/* Below will return a message to user if username taken, etc. Need to set this up. */}
             {res && <p className='danger'>{res.data.message}</p>}
+            <div className='sign in'>
+              Already have an account ?
+              <button type="button" className="btn btn-danger" onClick={() => {
+                setModalShow(false) // Close signup modal if open
+                setLoginModalShow(true) // Open login modal
+              }}>
+                log-in
+              </button>
+            </div>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          Already have an account ?
-          <button type="button" className="btn btn-danger" onClick={() => {
-    setModalShow(false) // Close signup modal if open
-    setLoginModalShow(true) // Open login modal
-  }}>
-  log-in
-</button>
-        </Modal.Footer>
       </Modal>
-      <Modal show={loginModalShow} halfscreen={true} onHide={() => setLoginModalShow(false)}>
+      <Modal show={loginModalShow} halfscreen={true} onHide={() => setLoginModalShow(false)} className='centered-modal'>
         <Modal.Header closeButton>
-          <Modal.Title>Log-in</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h1 className='text-center bold display-3 mb-4'>Login</h1>
-          <Form className='form' method='POST'>
-            <input type='email' name='email' placeholder='Email' />
-            <input type='password' name='password' placeholder='Password' />
+          <Form className='log_in' method='POST'>
+            <h1 className='text-center bold display-3 mb-4'>Login</h1>
+            <input type='email' name='email' placeholder='Email...' />
+            <input type='password' name='password' placeholder='Password...' />
             <button className='btn btn-grey' type='submit'>Login</button>
+            <button type="button" className="btn btn-danger" onClick={() => {
+              setModalShow(true)
+              setLoginModalShow(false)
+            }}>
+              sign up
+            </button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          
-          <button type="button" className="btn btn-danger" onClick={() => {
-    setModalShow(true)
-    setLoginModalShow(false) 
-  }}>
-  Back to sign up
-</button>
-        </Modal.Footer>
       </Modal>
     </>
   )
